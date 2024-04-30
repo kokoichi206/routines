9201fc0e4a0525df700ab12acf36483ca2e16d0f
kokoichi206 <tt.300607@gmail.com>
2024-04-30 22:38:39 +0900

    feat: workers から r2 に接続する

--- action_checker/cf-action-checkers/src/index.ts
+++ action_checker/cf-action-checkers/src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { stream } from "hono/streaming";
 
-const app = new Hono()
+// https://hono.dev/getting-started/cloudflare-workers#bindings
+type Bindings = {
+  MY_BUCKET: R2Bucket;
+  USERNAME: string;
+  PASSWORD: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/imgs/:username/:step", async (c, next) => {
+  const key = `${c.req.param("username")}/${c.req.param("step")}`;
+  console.log(`key: ${key}`);
+  const res = await c.env.MY_BUCKET.get(key);
+  if (res) {
+    c.header("Content-Type", "image/png");
+    // https://hono.dev/helpers/streaming
+    return stream(c, async (s) => {
+      s.pipe(res.body);
+    });
+  } else {
+    return c.notFound();
+  }
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
